2448aaaa3953a81fafc781517baf9762062c8dd9
Herraka10 <herrakapurnomo@gmail.com>
2025-07-25 15:15:07 +0700

    initialize new projext

---
+++ src/app/seo-friernly/metadata.ts
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Obito Uchiha (うちはオビト, Uchiha Obito) was a member of Konohagakure's Uchiha clan. He was believed to have died during the Third Shinobi World War, his only surviving legacy being the Sharingan he gave to his teammate,",
+    description: "Kakashi Hatake. In truth, Obito survived and was manipulated by Madara Uchiha into becoming his pawn. After being saved by Madara, Obito adopted the name Tobi (トビ, Tobi) and became a member of the Akatsuki, eventually becoming its leader.",
+    keywords: ["Obito Uchiha", "Akatsuki Leader", "Naruto", "Uchiha Clan", "Tobi", "Madara Uchiha"],
+    openGraph: {
+        title: "Obito Uchiha - Akatsuki Leader",
+        description: "Discover the story of Obito Uchiha, the Akatsuki leader who was once a member of Konohagakure's Uchiha clan. Learn about his transformation from a loyal shinobi to a key figure in the Akatsuki.",
+        images: 
+            "advance-next/src/app/seo-friernly/opengraph-image.jpg",
+            url: "https://www.obitou-uchiha-naruto.com",
+            siteName: "Naruto Universe",
+            locale: "id-ID",
+            type: "website",
+    }
+}
+
+export const size = {
+    width: 1200,
+    height : 630,
+}
---
+++ src/app/seo-friernly/page.tsx
@@ -0,0 +1,9 @@
+import { metadata } from "./metadata";
+export{ metadata } 
+export default function Page() {
+    return (
+        <>
+        <h1>test metadata</h1>
+        </>
+    );
+}
